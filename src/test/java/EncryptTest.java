243d0dd9e75027cbdfba5faa25f62f8dcdb8720a
Ted Mwangi <tedletheorde@gmail.com>
2020-06-01 22:44:40 +0300

    runned successful tests

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -6,9 +6,13 @@ public class EncryptTest {
 
     @Test
     public void getKey() {
+        Encrypt encrypt = new Encrypt(1, "Testfortherun");
+        assertEquals(1, encrypt.getKey());
     }
 
     @Test
     public void getSentence() {
+        Encoding encrypt = new Encrypt(1, "test123");
+        assertEquals("hey", encrypt.getSentence());
     }
 }
