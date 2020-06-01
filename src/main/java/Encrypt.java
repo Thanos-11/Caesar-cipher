4ed108c36682e07a72679869d5bcfbbf497485c8
Ted Mwangi <tedletheorde@gmail.com>
2020-06-01 22:26:26 +0300

    Added the first test

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,15 @@
 public class Encrypt {
+    private int key;
+    private String sentence;
+    public Encrypt(int key, String sentence){
+        this.key = key;
+        this.sentence= sentence;
+}
+    public int getKey() {
+        return key;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
 }
---
+++ src/test/java/EncryptTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EncryptTest {
+
+    @Test
+    public void getKey() {
+    }
+
+    @Test
+    public void getSentence() {
+    }
+}
