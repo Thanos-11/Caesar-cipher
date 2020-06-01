b1ebe53847022652f24908d44577381bdc690da6
Ted Mwangi <tedletheorde@gmail.com>
2020-06-02 00:50:08 +0300

    Added a gitignore file

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,15 +1,46 @@
 public class Encrypt {
-    private int key;
     private String sentence;
-    public Encrypt(int key, String sentence){
+    private int key;
+
+    public Encoding(String sentence, int key) {
+        this.sentence = sentence;
         this.key = key;
-        this.sentence= sentence;
-}
-    public int getKey() {
-        return key;
+
     }
 
     public String getSentence() {
         return sentence;
     }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String encrypt() {
+        StringBuilder encryptValue = new StringBuilder();
+        for (int i = 0; i<this.sentence.length();i++){
+            int character = (int)this.sentence.charAt(i);
+            if (Character.isUpperCase(character)) {
+                character = character + (this.key % 26);
+                if (character > 'Z') character = character - 26;
+            }
+            else if (Character.isLowerCase(character)) {
+                character = character + (this.key % 26);
+                if (character > 'z') character = character - 26;
+            }
+            encryptValue.append((char) character);
+
+        }
+        return encryptValue.toString();
+    }
 }
+
+
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -2,17 +2,19 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
+
 public class EncryptTest {
 
     @Test
     public void getKey() {
-        Encrypt encrypt = new Encrypt(1, "Testfortherun");
+        Encrypt encrypt = new Encrypt(1, "hey");
         assertEquals(1, encrypt.getKey());
     }
 
     @Test
     public void getSentence() {
-        Encoding encrypt = new Encrypt(1, "test123");
+        Encrypt encrypt = new Encrypt(1, "hey");
         assertEquals("hey", encrypt.getSentence());
     }
 }
