e0f0cc6662b3051eda3ae3a710182db5d581ec61
Ted Mwangi <tedletheorde@gmail.com>
2020-06-02 08:55:01 +0300

    ran the final tests

--- src/test/java/DecryptingTest.java
+++ src/test/java/DecryptingTest.java
@@ -6,29 +6,28 @@ public class DecryptingTest {
 
     @Test
     public void getKey() {
+            Decrypting test = new Decrypting(1, "Hi");
+            int expectedInt = 1;
+            assertEquals(expectedInt, Decrypting.getKey());
     }
 
     @Test
-    public void getSentence() {
+    public void getSentence_getCorrectSentence_String() {
+        Decrypting test = new Decrypting(1, "Hello");
+        String expectedOutput = "Hello";
+        assertEquals(expectedOutput,Decrypting.getSentence());
     }
 
     @Test
-    public void testGetSentence() {
+    public void setSentence_getCorrectSentence_String() {
+        Decrypting.setSentence("Hello");
+        assertEquals("Hello", Decrypting.getSentence());
     }
 
     @Test
-    public void testGetKey() {
-    }
-
-    @Test
-    public void setSentence() {
-    }
-
-    @Test
-    public void setKey() {
-    }
-
-    @Test
-    public void decrypt() {
+    public void test_decrypting_decryptString_string(){
+        Decrypting decrypting = new Decrypting(23,"QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD");
+        String expectedOuput = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        assertEquals(expectedOuput,decrypting.decrypt());
     }
 }
